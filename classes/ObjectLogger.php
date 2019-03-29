284c805d3cbdcdfb014f77e30ea0a3136fb8a5b0
FJ Rus <fjrus@webimpacto.es>
2019-03-29 08:39:45 +0100

    WEB-1669 Tercer Commit Modificacion errores

--- classes/ObjectLogger.php
+++ classes/ObjectLogger.php
@@ -1,21 +1,43 @@
 <?php
 
+/*
+* 2007-2016 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author fjrus <fjrus@webimpacto.com>
+*  @copyright  2007-2016 PrestaShop SA
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
  class ObjectLogger extends ObjectModel
   {
-
-
     public static $definition = array(
         'table' => 'objectlogguer',
         'primary' => 'id_objectlogguer',
         'fields' => array(
-            'affected_object' =>                array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'copy_post' => false),
-            'action_type' =>                    array('type' => self::TYPE_STRING, 'validate' => 'isName', 'size' => 255),
-            'object_type' =>                    array('type' => self::TYPE_STRING, 'validate' => 'isName', 'size' => 255),
-            'message' =>                        array('type' => self::TYPE_STRING, 'validate' => 'isGenericName'),
-            'date_add' =>                       array('type' => self::TYPE_DATE, 'validate' => 'isDate', 'copy_post' => false),
-            
+            'affected_object' => array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'copy_post' => false),
+            'action_type' => array('type' => self::TYPE_STRING, 'validate' => 'isName', 'size' => 255),
+            'object_type' => array('type' => self::TYPE_STRING, 'validate' => 'isName', 'size' => 255),
+            'message' => array('type' => self::TYPE_STRING, 'validate' => 'isGenericName'),
+            'date_add' => array('type' => self::TYPE_DATE, 'validate' => 'isDate', 'copy_post' => false), 
         ),
     );
-
-
 }
+
+?>
--- wim_objectlogguer.php
+++ wim_objectlogguer.php
@@ -1,5 +1,30 @@
 <?php
 
+/*
+* 2007-2016 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author fjrus <fjrus@webimpacto.com>
+*  @copyright  2007-2016 PrestaShop SA
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
   require_once ('classes/ObjectLogger.php');
   if (!defined('_PS_VERSION_'))
     exit;
@@ -26,11 +51,9 @@
  
     if (!Configuration::get('MYMODULE_NAME')) {      
       $this->warning = $this->l('No name provided');
-
     }
 
   }
-
 
     public function install()
     {    
@@ -55,15 +78,14 @@
 
       if(get_class($params['object']) != 'ObjectLogger') {
       $update->add();
+      }
     }
-    }
-
 
     public function hookActionObjectAddAfter($params)
     {
 
       $anadir = new ObjectLogger();
-      $anadirr->affected_object = $params['object']->id;
+      $anadir->affected_object = $params['object']->id;
       $anadir->action_type = 'Add';
       $anadir->object_type = get_class($params['object']);
       $anadir->message = "Object ". get_class($params['object']) . " with id " . $params['object']->id;
@@ -71,10 +93,8 @@
       
       if(get_class($params['object']) != 'ObjectLogger') {
       $anadir->add();
-
+      }
     }
-    }
-
 
     public function hookActionObjectDeleteAfter($params)
     {
@@ -85,9 +105,11 @@
       $del->object_type = get_class($params['object']);
       $del->message = "Object ". get_class($params['object']) . " with id " . $params['object']->id;
       $del->date_add = date("Y-m-d H:i:s");
+      
       if(get_class($params['object']) != 'ObjectLogger') {
       $del->add();
+      }
     }
-    }
-
   }
+
+  ?>
